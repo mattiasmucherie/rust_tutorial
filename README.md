778eaf8b86bba158286ccb87acb1a28e1f899417
mattiasmucherie <mattias.mattias.m@gmail.com>
2022-10-18 23:48:25 +0200

    functions

---
+++ README.md
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=ygL_xcavzQ4
+
+
+
+Following the tutorial on video above.
+
+Currently on 1:08:31. Chapter Generics.
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,7 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
+
 fn main() {
-    let vec1: Vec<i32> = Vec::new();
-    let mut vec2 = vec![1,123,3,4];
-    vec2.push(5);
-    let second: &i32 = &vec2[1];
-    match vec2.get(1) {
-        Some(second) => println!("2nd: {}", second),
-        None => println!("No second value...")
-    }
-
-
-    for i in &mut vec2 {
-        *i *= 2;
-    };
-    for i in &vec2 {
-        println!("{}",i);
-    };
+    
 }
