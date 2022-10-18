580950eda89c4f3d35455f2f1b4279c78fee464d
Mattias Mucherie <mattiasmucherie@pop-os.localdomain>
2022-10-18 23:29:18 +0200

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![allow(unused)]
+
+use std::io;
+use rand::{Rng, random};
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    impl Day {
+        fn is_weekend(&self) -> bool {
+            match self {
+                Day::Saturday | Day::Sunday => true,
+                _ => false
+            }
+        }
+    }
+
+    let today: Day = Day::Sunday;
+    match today {
+        Day::Monday => println!("Everyone hates Monday"),
+        _  => println!("Everybody like this day.")
+    }
+    println!("Is today the weekend? {}" , today.is_weekend() );
 }
