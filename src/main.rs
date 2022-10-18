50cabdd914b482255a4a4c7fa86c8c28a1d4d492
mattiasmucherie <mattias.mattias.m@gmail.com>
2022-10-18 23:37:01 +0200

    vectors

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,20 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    enum Day {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
-    }
-    impl Day {
-        fn is_weekend(&self) -> bool {
-            match self {
-                Day::Saturday | Day::Sunday => true,
-                _ => false
-            }
-        }
+    let vec1: Vec<i32> = Vec::new();
+    let mut vec2 = vec![1,123,3,4];
+    vec2.push(5);
+    let second: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("2nd: {}", second),
+        None => println!("No second value...")
     }
 
-    let today: Day = Day::Sunday;
-    match today {
-        Day::Monday => println!("Everyone hates Monday"),
-        _  => println!("Everybody like this day.")
-    }
-    println!("Is today the weekend? {}" , today.is_weekend() );
+
+    for i in &mut vec2 {
+        *i *= 2;
+    };
+    for i in &vec2 {
+        println!("{}",i);
+    };
 }
