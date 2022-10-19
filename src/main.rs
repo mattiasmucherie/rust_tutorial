b10763ff98f20ed4d470b1e9ef3b4a76e7b35f45
Mattias Mucherie <mattias.mattias.m@gmail.com>
2022-10-19 10:07:40 +0200

    Struct

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,40 @@ use rand::{Rng, random};
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
-
+use std::ops::Add;
+use std::collections::HashMap;
 
 fn main() {
+    const PI: f32 = 3.141592;
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
+
+    }
+
+    struct Rectangle {length: f32, width: f32};
+    struct Circle {length: f32, width: f32};
+    impl Shape for Rectangle{
+      fn new(length: f32 , width: f32) -> Rectangle {
+          return Rectangle{length, width};
+      }
+      fn area(&self) -> f32 {
+          return self.length * self.width;
+      }
+    };
+
+    impl Shape for Circle {
+        fn new(length: f32 , width: f32) -> Circle {
+            return Circle{length, width};
+        }
+        fn area(&self) -> f32 {
+            return (self.length /2.0).powf(2.0) * PI;
+        }
+    };
+
+    let rec: Rectangle = Shape::new(10.0,10.0);
+    let circ: Circle = Shape::new(10.0,10.0);
+    println!("Rec Area: {}", rec.area());
+    println!("Circ Area: {}", circ.area());
     
 }
